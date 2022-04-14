3cee609a44be59551f39a11f143a867c69e428ea
KevinPeraltaF <micontrasea99>
2022-04-13 20:17:57 -0500

    add login

--- odontologico/urls.py
+++ odontologico/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from odontologico.views import login_usuario
+from odontologico.views import login_usuario, dashboard
 
 urlpatterns = [
-    path(r'login/',login_usuario, name='login_usuario'),
+    path(r'login/', login_usuario, name='login_usuario'),
+    path(r'', dashboard, name='dashboard'),
 ]
--- odontologico/views.py
+++ odontologico/views.py
@@ -1,6 +1,11 @@
+import sys
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.db import transaction
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
+
 
 # Create your views here.
 @transaction.atomic()
@@ -12,15 +17,76 @@ def login_usuario(request):
             peticion = request.POST['peticion']
             if peticion == 'login_usuario':
                 try:
+                    usuario = authenticate(username=request.POST['usuario'].lower().strip(),
+                                           password=request.POST['clave'])
+                    if usuario is not None:
+                        if usuario.is_active:
+                            login(request, usuario)
+                        else:
+                            return JsonResponse(
+                                {"result": False, 'mensaje': u'Inicio de sesión incorrecto, usuario no activo.'})
+                    else:
+                        return JsonResponse(
+                            {"result": False, 'mensaje': u'Inicio de sesiión incorrecto, usuario incorrecto.'})
+
                     return JsonResponse({"respuesta": True}, safe=False)
                 except Exception as ex:
                     transaction.set_rollback(True)
-                    return JsonResponse({"respuesta": False, "mensaje": "Error al iniciar sesión, intentelo más tarde."})
+                    return JsonResponse(
+                        {"respuesta": False, "mensaje": "Error al iniciar sesión, intentelo más tarde."})
 
         return JsonResponse({"respuesta": False, "mensaje": "acción Incorrecta."})
     else:
         if 'peticion' in request.GET:
             peticion = request.GET['peticion']
+            if peticion == 'peticion':
+                try:
+                    pass
+                except Exception as ex:
+                    transaction.set_rollback(True)
+                    pass
+
         else:
-            pass
-    return render(request, "registration/login.html", data)
+            try:
+                data['titulo'] = 'Inicio de sesión'
+                return render(request, "registration/login.html", data)
+            except Exception as ex:
+                print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+
+
+
+
+
+@login_required(redirect_field_name='next', login_url='/login')
+@transaction.atomic()
+def dashboard(request):
+    global ex
+    data = {}
+    if request.method == 'POST':
+        if 'peticion' in request.POST:
+            peticion = request.POST['peticion']
+
+            return JsonResponse({"respuesta": False, "mensaje": "acción Incorrecta."})
+        else:
+            if 'peticion' in request.GET:
+                peticion = request.GET['peticion']
+
+    else:
+        if 'peticion' in request.GET:
+            peticion = request.GET['peticion']
+            if peticion == 'peticion':
+                try:
+                    pass
+                except Exception as ex:
+                    transaction.set_rollback(True)
+                    pass
+
+        else:
+            try:
+                data['titulo'] = 'Dashboard'
+                return render(request, "registration/dashboard.html ", data)
+            except Exception as ex:
+                print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+
+
+
