cee7f164375b8b731f297fa6565ed8fac04be127
KevinPeraltaF <micontrasea99>
2022-09-03 14:05:51 -0500

    graficos estadisticos

--- odontologico/view_reportes.py
+++ odontologico/view_reportes.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 
 from odontologico.forms import PersonaForm
 from odontologico.funciones import add_data_aplication
-from odontologico.models import Persona, Doctor
+from odontologico.models import Persona, Doctor, Paciente, Tratamiento, Consulta
 
 
 @login_required(redirect_field_name='next', login_url='/login')
@@ -56,7 +56,24 @@ def view_reportes(request):
             try:
                 data['titulo'] = 'Reportes'
                 data['persona_logeado'] = persona_logeado
+                data['pacientes_masculino']  = pacientes_masculino = Paciente.objects.filter(status=True, persona__genero__pk = 1).count()
+                data['pacientes_femenino'] =pacientes_femenino = Paciente.objects.filter(status=True, persona__genero__pk = 2).count()
 
+                #pacientes por tratamientos
+                data['tratamientos'] = tratamientos = Tratamiento.objects.filter(status = True)
+                data['consultas'] =consultas = Consulta.objects.raw('SELECT trat.id, trat.nombre, ( SELECT  COUNT(c.consulta_id) AS paciente FROM odontologico_consulta_tratamientos c WHERE c.tratamiento_id=trat.id group by c.tratamiento_id ) AS cantidad_pacientes FROM  odontologico_tratamiento trat ')
+                tratamientos = []
+                cantidad_pacientes = []
+                for c in consultas:
+
+                    tratamientos.append(c.nombre)
+                    if c.cantidad_pacientes is None:
+                        valor = 0
+                    else:
+                        valor =c.cantidad_pacientes
+                    cantidad_pacientes.append(valor)
+                data['tratamientos'] =tratamientos
+                data['cantidad_pacientes'] =cantidad_pacientes
                 return render(request, "reportes/view.html", data)
             except Exception as ex:
                 print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
